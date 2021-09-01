2baa117077a1f95051e1bc3ed3600dbdb4d13a04
BoraldPelmenier@gmail.com <58140350+BoraldPelmenier@users.noreply.github.com>
2021-09-01 17:51:42 +0700

    first commit

---
+++ WindowsFormsAppWebApi/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace WindowsFormsAppWebApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textUserId = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textResponse = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textUserId
+            // 
+            this.textUserId.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textUserId.Location = new System.Drawing.Point(12, 12);
+            this.textUserId.Name = "textUserId";
+            this.textUserId.Size = new System.Drawing.Size(399, 20);
+            this.textUserId.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(437, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send request";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textResponse
+            // 
+            this.textResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textResponse.Location = new System.Drawing.Point(13, 39);
+            this.textResponse.Name = "textResponse";
+            this.textResponse.Size = new System.Drawing.Size(398, 218);
+            this.textResponse.TabIndex = 2;
+            this.textResponse.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 273);
+            this.Controls.Add(this.textResponse);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textUserId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textUserId;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox textResponse;
+    }
+}
+
---
+++ WindowsFormsAppWebApi/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppWebApi
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient _client;
+        private string ACCESS_TOKEN = "27eed51727eed51727eed5172b2797ff67227ee27eed51746c1941a225751a650a6839b";
+        private string VERSION = "5.131";
+        public Form1()
+        {
+            InitializeComponent();
+            _client = new HttpClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string JsonPretty(string unPrettyJson)
+        {
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(unPrettyJson);
+
+            return JsonSerializer.Serialize(jsonElement, options);
+        }
+        async void FetchUserInfo()
+        {
+            HttpResponseMessage response = await _client.GetAsync($"https://api.vk.com/method/users.get?user_ids={textUserId.Text}&access_token={ACCESS_TOKEN}&v={VERSION}");
+            textResponse.Text = JsonPretty(await response.Content.ReadAsStringAsync());
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FetchUserInfo();
+            textResponse.Text = "Loading...";
+        }
+    }
+}
